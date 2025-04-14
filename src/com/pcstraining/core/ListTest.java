42e68b1ceda72bc76fb638657126eb5fba714d17
anand6188 <anandkamble6188@gmail.com>
2025-04-14 15:54:42 +0530

    List test file adde

---
+++ src/com/pcstraining/core/ListTest.java
@@ -0,0 +1,15 @@
+package com.pcstraining.core;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListTest {
+    public static void main(String[] args) {
+        List <String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Grapes");
+        System.out.println(list);
+    }
+}
