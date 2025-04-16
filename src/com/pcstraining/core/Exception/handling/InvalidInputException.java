cacd41bdec6423737b911c26f423de9d7bd682ea
anand6188 <anandkamble6188@gmail.com>
2025-04-16 22:46:11 +0530

    Exception

---
+++ src/com/pcstraining/core/Exception/handling/InvalidInputException.java
@@ -0,0 +1,14 @@
+package com.pcstraining.core.Exception.handling;
+
+public class InvalidInputException extends RuntimeException{ //
+    private final String message;
+
+    public InvalidInputException(String message){
+        super();
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
---
+++ src/com/pcstraining/core/Exception/handling/TestEnhancedTryCloseCall.java
@@ -0,0 +1,16 @@
+package com.pcstraining.core.Exception.handling;
+
+public class TestEnhancedTryCloseCall implements AutoCloseable {
+    public static void main(String[] args) {
+        try (TestEnhancedTryCloseCall testEnhancedTryCloseCall = new TestEnhancedTryCloseCall()){
+            System.out.println("in try block");
+        } catch (Exception e) {
+            System.out.println("in catch block");
+            throw new RuntimeException(e);
+        }
+    }
+    @Override
+    public void close() throws Exception {
+        System.out.println("closing TestFinallyClosableCall");
+    }
+}
